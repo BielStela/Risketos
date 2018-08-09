017f20a85749771251d7c85226501c0890860942
BielStela <biel.stela@gmail.com>
2018-08-09 11:25:54 +0200

    updated player

--- base_game/player.py
+++ base_game/player.py
@@ -1,7 +1,22 @@
 class Player():
     """ Player base class """
 
-    def __init__(player_name: str, armies: dict):
+    def __init__(self, player_name: str, armies: dict, objective):
         raise NotImplementedError
+        self.player_name = player_name
+        self.armies = armies
+        
+        self.objective = 
 
+    def ressuply_troops(self):
+        pass
+    
+    def attack(self):
+        pass
+    
+    def fortify(self):
+        pass
+    
+    def pickup_card(self):
+        pass
     
